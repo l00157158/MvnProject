7cc81e439d4c410f9a24757c31ba1c16fa161a87
L00157158 <L00157158@atu.ie>
2023-10-18 23:03:01 +0100

    mvn project testing

---
+++ demo/src/main/java/com/example/Calculator.java
@@ -0,0 +1,9 @@
+// Calculator.java
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
---
+++ demo/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,26 @@
+// CalculatorTest.java
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(3, 2);
+
+        assertEquals("The sum should be 5", 5, sum);
+    }
+
+    @Test
+    public void testAddFailure() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(2, 3);
+
+        // This will fail because 2 + 2 is 4, not 5
+        assertEquals("The sum should be 5", 5, sum);
+    }
+}
