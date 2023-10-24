b8f3eb2b307d9970752d04e7f7a3302c706af956
L00157158 <L00157158@atu.ie>
2023-10-25 00:24:44 +0100

    to test the build

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -10,6 +10,11 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Hello Jimmy");
-        
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(40, 50);
+        int diff = calculator.substract(50, 40);
+
+        System.out.println("sum: " + sum);
+        System.out.print("diff: " + diff);
     }
 }
--- demo/src/main/java/com/example/Calculator.java
+++ demo/src/main/java/com/example/Calculator.java
@@ -5,5 +5,8 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+    public int substract(int a, int b){
+        return a -b ;
+    }
 }
-
+// just adding a comment
